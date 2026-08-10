4bfbe19c11ced841e00bd57f6780caca1e4bd138
NJ <notsolustig3@gmail.com>
2026-08-10 12:42:20 +0200

    added

--- WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -94,7 +94,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button2
             // 
@@ -106,7 +106,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button3
             // 
@@ -118,7 +118,7 @@
             this.button3.TabIndex = 5;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button4
             // 
@@ -130,7 +130,7 @@
             this.button4.TabIndex = 6;
             this.button4.Text = "-";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.EventClickArithmeticHandler);
             // 
             // button5
             // 
@@ -142,7 +142,7 @@
             this.button5.TabIndex = 10;
             this.button5.Text = "*";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.Click += new System.EventHandler(this.EventClickArithmeticHandler);
             // 
             // button6
             // 
@@ -154,7 +154,7 @@
             this.button6.TabIndex = 9;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button7
             // 
@@ -166,7 +166,7 @@
             this.button7.TabIndex = 8;
             this.button7.Text = "5";
             this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button8
             // 
@@ -178,7 +178,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "4";
             this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button9
             // 
@@ -190,7 +190,7 @@
             this.button9.TabIndex = 14;
             this.button9.Text = "/";
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.Click += new System.EventHandler(this.EventClickArithmeticHandler);
             // 
             // button10
             // 
@@ -202,7 +202,7 @@
             this.button10.TabIndex = 13;
             this.button10.Text = "9";
             this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click);
+            this.button10.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button11
             // 
@@ -214,7 +214,7 @@
             this.button11.TabIndex = 12;
             this.button11.Text = "8";
             this.button11.UseVisualStyleBackColor = true;
-            this.button11.Click += new System.EventHandler(this.button11_Click);
+            this.button11.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button12
             // 
@@ -226,7 +226,7 @@
             this.button12.TabIndex = 11;
             this.button12.Text = "7";
             this.button12.UseVisualStyleBackColor = true;
-            this.button12.Click += new System.EventHandler(this.button12_Click);
+            this.button12.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button13
             // 
@@ -238,7 +238,7 @@
             this.button13.TabIndex = 18;
             this.button13.Text = "+";
             this.button13.UseVisualStyleBackColor = true;
-            this.button13.Click += new System.EventHandler(this.button13_Click);
+            this.button13.Click += new System.EventHandler(this.EventClickArithmeticHandler);
             // 
             // button14
             // 
@@ -262,7 +262,7 @@
             this.button15.TabIndex = 16;
             this.button15.Text = "0";
             this.button15.UseVisualStyleBackColor = true;
-            this.button15.Click += new System.EventHandler(this.button15_Click);
+            this.button15.Click += new System.EventHandler(this.EventClickNumHandler);
             // 
             // button16
             // 
@@ -286,7 +286,7 @@
             this.button19.TabIndex = 20;
             this.button19.Text = "=";
             this.button19.UseVisualStyleBackColor = true;
-            this.button19.Click += new System.EventHandler(this.button19_Click);
+            this.button19.Click += new System.EventHandler(this.Gleich);
             // 
             // button17
             // 
--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -35,14 +35,24 @@ namespace WindowsFormsApp2
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void EventClickNumHandler(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + "1";
+            var button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void EventClickArithmeticHandler(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + "2";
+            var aributton = (Button)sender;
+
+            if (textBox1.Text.Length == 0) return;
+
+            value1 = Convert.ToDouble(textBox1.Text);
+            sign = aributton.Text;
+            label1.Text = textBox1.Text + sign;
+            textBox1.Text = "";
+
+            history.Add(value1);
         }
 
         private void button16_Click(object sender, EventArgs e)
@@ -53,91 +63,8 @@ namespace WindowsFormsApp2
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void Gleich(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + "3";
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + "4";
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + "5";
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + "6";
-        }
-
-        private void button12_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + "7";
-        }
-
-        private void button11_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + "8";
-        }
-
-        private void button10_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + "9";
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + "0";
-        }
-
-        private void button13_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text.Length == 0) return;
-
-            value1 = Convert.ToDouble(textBox1.Text);
-            sign = "+";
-            label1.Text = textBox1.Text + sign;
-            textBox1.Text = "";
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text.Length == 0) return;
-
-            value1 = Convert.ToDouble(textBox1.Text);
-            sign = "-";
-            label1.Text = textBox1.Text + sign;
-            textBox1.Text = "";
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text.Length == 0) return;
-
-            value1 = Convert.ToDouble(textBox1.Text);
-            sign = "*";
-            label1.Text = textBox1.Text + sign;
-            textBox1.Text = "";
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text.Length == 0) return;
-
-            value1 = Convert.ToDouble(textBox1.Text);
-            sign = "/";
-            label1.Text = textBox1.Text + sign;
-            textBox1.Text = "";
-        }
-
-        private void button19_Click(object sender, EventArgs e)
-        {
-
-
-
 
             if (textBox1.Text.Length == 0) return;
 
@@ -217,7 +144,7 @@ namespace WindowsFormsApp2
             using (XmlWriter writer = XmlWriter.Create(dateipfad, new XmlWriterSettings() { Indent = true }))
             {
                 writer.WriteStartDocument();
-                
+
                 writer.WriteStartElement("Datenbank");
                 writer.WriteStartElement("History");
                 foreach (var item in history)
